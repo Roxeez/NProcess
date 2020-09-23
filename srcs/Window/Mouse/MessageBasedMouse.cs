244eb0d07715b36deeb0db63fcaa0b950c90c9b8
Roxeez <roxeeztsu@gmail.com>
2020-09-23 17:21:01 +0200

    fix typo

--- srcs/Window/Mouse/MessageBasedMouse.cs
+++ srcs/Window/Mouse/MessageBasedMouse.cs
@@ -13,22 +13,22 @@ namespace NProcess.Window.Mouse
             this.handle = handle;
         }
         
-        public void LeftClick(Position point)
+        public void LeftClick(Position position)
         {
-            User32.PostMessage(handle, WindowsMessage.LeftButtonDown, new UIntPtr(0x00000001), CreateLParam(point.X, point.Y));
-            User32.PostMessage(handle, WindowsMessage.LeftButtonUp, new UIntPtr(0x00000000), CreateLParam(point.X, point.Y));
+            User32.PostMessage(handle, WindowsMessage.LeftButtonDown, new UIntPtr(0x00000001), CreateLParam(position.X, position.Y));
+            User32.PostMessage(handle, WindowsMessage.LeftButtonUp, new UIntPtr(0x00000000), CreateLParam(position.X, position.Y));
 ;        }
 
-        public void RightClick(Position point)
+        public void RightClick(Position position)
         {
-            User32.PostMessage(handle, WindowsMessage.RightButtonDown, new UIntPtr(0x00000001), CreateLParam(point.X, point.Y));
-            User32.PostMessage(handle, WindowsMessage.RightButtonUp, new UIntPtr(0x00000000), CreateLParam(point.X, point.Y));
+            User32.PostMessage(handle, WindowsMessage.RightButtonDown, new UIntPtr(0x00000001), CreateLParam(position.X, position.Y));
+            User32.PostMessage(handle, WindowsMessage.RightButtonUp, new UIntPtr(0x00000000), CreateLParam(position.X, position.Y));
         }
 
-        public void MiddleClick(Position point)
+        public void MiddleClick(Position position)
         {
-            User32.PostMessage(handle, WindowsMessage.MiddleButtonDown, new UIntPtr(0x00000001), CreateLParam(point.X, point.Y));
-            User32.PostMessage(handle, WindowsMessage.MiddleButtonUp, new UIntPtr(0x00000000), CreateLParam(point.X, point.Y));
+            User32.PostMessage(handle, WindowsMessage.MiddleButtonDown, new UIntPtr(0x00000001), CreateLParam(position.X, position.Y));
+            User32.PostMessage(handle, WindowsMessage.MiddleButtonUp, new UIntPtr(0x00000000), CreateLParam(position.X, position.Y));
         }
         
         private static UIntPtr CreateLParam(int x, int y)
